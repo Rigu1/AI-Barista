3d05be512c3c449dd96c69a72ce2e94a15be5ff7
rigu1 <dlsrbtla@gmail.com>
2026-06-10 00:47:11 +0900

    refactor: 중복되는 로직 제거

--- analyzer_server.py
+++ analyzer_server.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import librosa
-import soundfile as sf
+import numpy as np
 import os
 import warnings
 from transformers import pipeline
@@ -12,9 +12,9 @@ warnings.filterwarnings("ignore")
 app = FastAPI()
 
 print("="*50)
-print("[FastAPI] 모델을 메모리로 옮기는 중...")
-pipe = pipeline("audio-classification", model="dima806/music_genres_classification")
-print("[FastAPI] 로딩 완료")
+print("[FastAPI] 모델 로딩 시작...")
+pipe = pipeline("audio-classification", model="michaelfeil/audio-classification-music-genres", device=-1)
+print("[FastAPI] 모델 로딩 완료")
 print("="*50)
 
 class AnalyzeRequest(BaseModel):
@@ -24,24 +24,19 @@ class AnalyzeRequest(BaseModel):
 
 @app.post("/analyze")
 async def analyze_audio(req: AnalyzeRequest):
-    print(f"[FastAPI] DEBUG: 수신한 데이터: {req}")
+    print(f"[FastAPI] 분석 요청 접수: {req.filepath} ({req.start_time}s ~ {req.start_time + req.duration}s)")
     try:
         y, sr = librosa.load(req.filepath, sr=16000, offset=req.start_time, duration=req.duration)
-        temp_path = f"temp_fastapi_{os.getpid()}.wav"
-        sf.write(temp_path, y, sr)
         
-        results = pipe(temp_path)
+        results = pipe(y)
         
-        if os.path.exists(temp_path):
-            os.remove(temp_path)
-            
-        output_dict = {res['label']: res['score'] for res in results}
+        output_dict = {res['label']: float(res['score']) for res in results}
+        
         return output_dict
         
     except Exception as e:
-        print(f"[FastAPI]: Error occurred: {str(e)}")
+        print(f"[FastAPI] 분석 중 에러 발생: {str(e)}")
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
-    print(f"✨ [FastAPI] 분석 서버 가동")
