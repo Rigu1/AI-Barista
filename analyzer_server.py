e5436a99b420f38391ff1d076fdecf914b3ed415
rigu1 <dlsrbtla@gmail.com>
2026-06-10 02:11:18 +0900

    feat: 데이터 분석 구간 수 줄이기

--- analyzer_server.py
+++ analyzer_server.py
@@ -25,21 +25,20 @@ class AnalyzeRequest(BaseModel):
 @app.post("/analyze")
 async def analyze_audio(req: AnalyzeRequest):
     try:
-        segments = [0.0, (req.duration - 3.0) / 2, req.duration - 3.0]
+        segments = [0.0, req.duration - 5.0]
         
         final_scores = {}
         sr = 16000
         
         for start in segments:
-
-            y, _ = librosa.load(req.filepath, sr=sr, offset=req.start_time + start, duration=3.0)
+            y, _ = librosa.load(req.filepath, sr=sr, offset=req.start_time + start, duration=5.0)
             
             results = pipe(y, top_k=3)
             
             for res in results:
                 final_scores[res['label']] = final_scores.get(res['label'], 0) + res['score']
         
-        return {label: round(score / 3.0, 4) for label, score in final_scores.items()}
+        return {label: round(score / 2.0, 4) for label, score in final_scores.items()}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
